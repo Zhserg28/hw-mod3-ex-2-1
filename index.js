489f14bc4bcf38564eb929358dc3747ccf861f7b
Zhserg28 <serg280880@gmail.com>
2023-06-04 09:38:12 +0800

    hw_les_2

---
+++ index.js
@@ -0,0 +1,38 @@
+const yargs = require('yargs')
+const pkg = require('./package.json')
+const {addNote, listNotes, removeNotes} = require('./notes.controller')
+
+yargs.version(pkg.version)
+
+yargs.command({
+  command: 'add',
+  describe: 'Add new note to list',
+  builder: {
+    title: {
+      type: 'string',
+      describe: 'Note title',
+      demandOption: true
+    }
+  },
+  handler({ title }) {
+    addNote(title)
+  }
+})
+
+yargs.command({
+  command: 'list',
+  describe: 'Print all notes',
+  async handler() {
+    listNotes()
+  }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove note by id',
+    async handler({id}){
+        removeNotes(id)
+    }
+})
+
+yargs.parse()
